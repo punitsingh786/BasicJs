f79e26210f0fc7e138fe7450201106f967c01fbe
punitsingh786 <punitkumarsingh987@gmail.com>
2026-07-14 12:48:10 +0000

    config 01 lect

---
+++ chaiCode/01_basics.js
@@ -0,0 +1,19 @@
+const accountId=144553;
+let accountEmail="punitsingh@gmail.com";
+var accountPassword="12345";
+accountCitu="delhi";
+
+console.log(accountId);
+
+accountEmail="pun@gmail.com";
+accountPassword="54321";
+accountCity="delih";
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+prefer not to use var 
+beacause of issue in block scope and functional scope
+use let instead;
+*/
+
