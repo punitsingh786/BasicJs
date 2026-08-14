e52556a97607e09a3d905c7fef2ed7d2484a7cf6
punitsingh786 <punitkumarsingh987@gmail.com>
2026-08-14 16:53:32 +0000

    DOM SELECTORS NOTES

---
+++ 05_dom/01_domSelector.js
@@ -0,0 +1,7 @@
+//note1-->
+//some js selectors
+//1-->document.getElementById("id_name")-->return whole element;
+
+//A. document.getElementById("id_name").className;--->return class name of that particular id
+//B.document.getElementById('id_name').getAttribute('attributr_name');-->return the name of that attribute
+//C.document.getElementById('id_name').setAttribute("attribute_name","value")
